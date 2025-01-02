9e9a4daaf633ed6fc5da774e7aa1fa610e43c3ad
Murillo Carlos <cmb2897@gmail.com>
2025-01-02 00:01:29 -0600

    Adding images and initial Navbar.

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 
+import { images } from '../../constants/';
 import './Navbar.scss';
+
+const items = [
+    'home', 'about', 'contact', 'work', 'skills', 'contact'
+];
 
 const Navbar = ()  => {
     return (
-        <div>Navbar</div>
+        <nav className="app__navbar">
+            <div className="app__navbar-logo">
+                <img src={images.logo} alt="logo" />
+            </div>
+            <ul className="app__navbar-links">
+                {
+                items.map((item) => (
+                    <li key={`link-${item}`} className="app__flex p-text">
+                        <div />
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     );
 }
 
