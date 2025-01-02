6f1eb032356e46e795f0288824658721abf315be
Murillo Carlos <cmb2897@gmail.com>
2025-01-01 18:03:28 -0600

    Adding Schema Types for common entities.

---
+++ backend_sanity/schemaTypes/index.ts
@@ -0,0 +1,12 @@
+import works from "./works";
+import abouts from "./abouts";
+import brands from "./brands";
+import skills from "./skills";
+import contact from "./contact";
+import experiences from "./experiences";
+import testimonials from "./testimonials";
+import workExperience from "./workExperience";
+
+export const schemaTypes = [
+    works, testimonials, brands, abouts, skills, workExperience, experiences, contact
+];
